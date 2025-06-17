53838949e386ae384db802faaad5b6af9480ac13
view-village <Naphat.J@monkeytech.co.th>
2025-06-17 17:11:57 +0700

    add new model and type

--- Hitop_api/hitop.app.service/HitopService.cs
+++ Hitop_api/hitop.app.service/HitopService.cs
@@ -33,13 +33,13 @@ namespace hitop.app.service
         }
         productModel[] testProduct = new productModel[]
 {
-            new productModel { id = 1, code = "1", name = "product1" },
-            new productModel { id = 2, code = "2", name = "product2" },
-            new productModel { id = 3, code = "3", name = "product3" },
-            new productModel { id = 4, code = "4", name = "product4" },
-            new productModel { id = 5, code = "5", name = "product5" },
-            new productModel { id = 6, code = "6", name = "product6" },
-            new productModel { id = 7, code = "7", name = "product7" }
+            new productModel { id = 1, code = "1", name = "product1", price = 10, description = "1111111",type = "Food" },
+            new productModel { id = 2, code = "2", name = "product2", price = 20, description = "2222222",type = "Drink" },
+            new productModel { id = 3, code = "3", name = "product3", price = 30, description = "3333333",type = "Food" },
+            new productModel { id = 4, code = "4", name = "product4", price = 40, description = "4444444",type = "Food" },
+            new productModel { id = 5, code = "5", name = "product5", price = 50, description = "5555555",type = "Drink" },
+            new productModel { id = 6, code = "6", name = "product6", price = 60, description = "6666666",type = "Drink" },
+            new productModel { id = 7, code = "7", name = "product7", price = 70, description = "7777777",type = "Food" }
         };
 
 
@@ -54,6 +54,14 @@ namespace hitop.app.service
         
             IEnumerable<productModel> iens = null;
             iens = testProduct;
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                iens = testProduct
+                    .Where(p => p.name.Contains(search, StringComparison.OrdinalIgnoreCase))
+                    .ToArray();
+            }
+
             return iens;
         }
 
--- Hitop_api/hitop.model/productModel.cs
+++ Hitop_api/hitop.model/productModel.cs
@@ -5,6 +5,9 @@
         public int id { get; set; }
         public string code { get; set; }
         public string name { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
 
     }
 }
